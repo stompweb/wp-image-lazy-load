9eca03b8c73e80f8cb52de66d9f3224e49173762
Steven Jones <hello@stomptheweb.co.uk>
2016-03-25 22:52:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+WP Lazy Load
+=====================
+
+A simple lazy load solution for pages and posts
+
+## Usage
+
+* Activate Plugin
+* Win
+
+## Lazy Loading
+
+Images are the key cause of large page sizes. They are often loaded even when not needed causing page sizes to be bigger than they need to be. This plugin loads images only when the user scrolls to it on the site. This plugin can reduce page load times and bandwidth usage.
+
+## About the plugin
+
+The plugin filters the_content on pages and posts; changing the output of the <img> tag to work with a third party script. 
+
+http://afarkas.github.io/lazysizes/
+
+The plugin only works on the_content and does not work on all images across the site. This is to keep things simple and is usually the main area in which theme developers do not have control over the output.
---
+++ lazy-load.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: WP Lazy Load
+Plugin URI: http://stomptheweb.co.uk
+Description: Lazy Load images
+Version: 1.0.0
+Author: Steven Jones
+Author URI: http://stomptheweb.co.uk/
+License: GPL2
+*/
+
+include 'lib/simple_html_dom.php';
+
+function wp_register_lazy_load_script() {
+	
+	wp_register_script( 'lazy-load', plugin_dir_url( __FILE__ ) . 'js/lazysizes.min.js', array( 'jquery'), NULL, true);
+
+	if (is_singular()) {
+		wp_enqueue_script( 'lazy-load' );
+	}
+	
+}
+add_action('wp_enqueue_scripts', 'wp_register_lazy_load_script');
+
+function wp_filter_img_tags($content) {
+
+	if (!is_singular() || is_front_page()) {
+		return $content;
+	}
+
+	$html = str_get_html($content, '', '', '', false);
+	$placeholder = 'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==';
+
+    foreach ($html->find('img') as $element) {
+
+        // Add lazyload class
+        $element->class = 'lazyload ' . $element->class;
+        
+        // Populate data attribute
+        $element->{'data-srcset'} = $element->srcset;
+        $element->{'data-src'} = $element->src;
+
+        // Remove standard image properties
+        $element->{'src'} = $placeholder;
+    	$element->{'srcset'} = '';
+    }
+
+	return $html;
+
+}
+add_filter('the_content', 'wp_filter_img_tags', 99);
